59029d89a81cfb13048b7509bef72db65d9ed09a
Batfino <151618376+Batfino@users.noreply.github.com>
2025-12-05 13:04:33 +0100

    movement

---
+++ Tower of Dunqeon/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float MoveSpeed = 7f;
+
+    Rigidbody rb;
+
+    Vector3 move;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // to prevent physics from tipping the object over
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hx = Input.GetAxisRaw("Horizontal");
+        float vz = Input.GetAxisRaw("Vertical");
+        
+        Vector3 input = new Vector3(hx, 0f, vz);
+        move = input.normalized;
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = move * MoveSpeed + new Vector3(0f, rb.velocity.y, 0f);
+    }
+}
