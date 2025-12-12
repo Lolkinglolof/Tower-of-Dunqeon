44e6fd23c5e6357e34be6b66468c07585240526b
Lolkinglolof <Torben.Thiemer@gmail.com>
2025-12-12 14:00:39 +0100

    Update EnemyBehavior.cs

--- Tower of Dunqeon/Assets/Scripts/Enemy things/EnemyBehavior.cs
+++ Tower of Dunqeon/Assets/Scripts/Enemy things/EnemyBehavior.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    private bool goalReached = false;
+    private bool goalReached = true;
     private float moveSpeed = 1;
     private Vector3 goal;
+    private Rigidbody body;
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,11 +20,27 @@ public class EnemyBehavior : MonoBehaviour
             goalReached = false;
             float angle = Random.Range(0, 359.9f);
             float distance = Random.Range(0, 5);
-            goal = Quaternion.AngleAxis(angle, Vector3.up) * new Vector3(distance, 0, 0);
+            goal = Quaternion.AngleAxis(angle, Vector3.up) * new Vector3(distance, 0, 0) + transform.position;
         }
-        else
+    }
+    private void FixedUpdate()
+    {
+        if (!goalReached)
         {
-
+            Vector3 direction = goal - gameObject.transform.position;
+            direction.y = 0;
+            body.velocity = direction.normalized * moveSpeed * Time.deltaTime;
+            if ((goal - gameObject.transform.position).magnitude < new Vector3(1, 1, 1).magnitude)
+            {
+                goalReached = true;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            goalReached = true;
         }
     }
 }
