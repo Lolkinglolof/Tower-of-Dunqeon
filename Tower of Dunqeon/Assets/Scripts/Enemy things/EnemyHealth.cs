4821881706edeb445bf302f29a6d395f535c356f
Lolkinglolof <Torben.Thiemer@gmail.com>
2026-01-05 15:08:08 +0100

    yeet 2

--- Tower of Dunqeon/Assets/Scripts/Enemy things/EnemyHealth.cs
+++ Tower of Dunqeon/Assets/Scripts/Enemy things/EnemyHealth.cs
@@ -4,7 +4,10 @@ public class EnemyHealth : Health
     {
         HealthAmount = 50;
     }
-
+    public override void OnTakeDamage(int DamageAmount)
+    {
+        base.OnTakeDamage(DamageAmount);
+    }
     override public void OnDeath()
     {
         gameObject.SetActive(false);
