ac8675675aba6033d49cdac72b61da2f6be3232a
Lolkinglolof <Torben.Thiemer@gmail.com>
2026-01-05 14:38:14 +0100

    Update EnemyBehavior.cs

--- Tower of Dunqeon/Assets/Scripts/Enemy things/EnemyBehavior.cs
+++ Tower of Dunqeon/Assets/Scripts/Enemy things/EnemyBehavior.cs
@@ -2,19 +2,32 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    [SerializeField] LayerMask wallMask;
     private bool goalReached = true;
-    private float moveSpeed = 1;
+    private float moveSpeed = 1, detectionRange = 5, engagementRange = 1;
     private Vector3 goal;
     private Rigidbody body;
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.transform.position.magnitude - transform.position.magnitude < detectionRange)
+        {
+            Vector3 rayDirection = (player.transform.position - transform.position).normalized;
+            if (!Physics.Raycast(transform.position, rayDirection, detectionRange, wallMask))
+            {
+                goalReached = false;
+                goal = player.transform.position;
+            }
+        }
         if (goalReached)
         {
             goalReached = false;
@@ -29,9 +42,9 @@ public class EnemyBehavior : MonoBehaviour
         {
             Vector3 direction = goal - gameObject.transform.position;
             direction.y = 0;
-            Vector3 movementVector = direction.normalized * moveSpeed * Time.deltaTime
-            body.addForce(movementVector);
-            if ((goal - gameObject.transform.position).magnitude < new Vector3(1, 1, 1).magnitude)
+            Vector3 movementVector = direction.normalized * moveSpeed * Time.deltaTime;
+            body.AddForce(movementVector);
+            if ((goal - gameObject.transform.position).magnitude < engagementRange)
             {
                 goalReached = true;
             }
@@ -39,7 +52,7 @@ public class EnemyBehavior : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "wall")
+        if (collision.gameObject.CompareTag("wall"))
         {
             goalReached = true;
         }
